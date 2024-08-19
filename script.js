c7f32cea755d7d58dee23b925bdc2645992b193d
Vivek01Kaloori <136892525+VivekBannu@users.noreply.github.com>
2024-08-19 21:27:06 +0530

    html-portfolio2024

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu()
+{
+    var menu = document.querySelector(".menu-links");
+    var icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    console.log("helloooo");
+}
